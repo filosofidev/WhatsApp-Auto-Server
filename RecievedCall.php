3bc2632bbd76cc35564237bbbecd6d4a414b524f
Tangguh Filosofi Ugra N <filosofidev@gmail.com>
2025-12-13 09:31:46 +0700

    Rev update call
    Added echo statement to output the response from the API call.

--- RecievedCall.php
+++ RecievedCall.php
@@ -9,7 +9,7 @@ curl_setopt_array($curl, array(
   CURLOPT_MAXREDIRS => 10,
   CURLOPT_TIMEOUT => 0,
   CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_Call => true,
+  CURLOPT_CALL => true,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => 'POST',
   CURLOPT_POSTFIELDS => array('device_id' => '3b406fa2eb5a1dd20f6e7d81e846b6500','callin' => 'Masukkan gmail yang sinkron dengan kontak WhatsApp'),
@@ -18,4 +18,5 @@ curl_setopt_array($curl, array(
 $response = curl_exec($curl);
 
 curl_close($curl);
+
 echo $response;
